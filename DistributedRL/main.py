bbe8d22d0ca320c0f30e9cb0b4bcddda45b75624
Rimika <rimikadhara67@gmail.com>
2025-01-17 12:35:09 -0600

    Updated Main

--- DistributedRL/main.py
+++ DistributedRL/main.py
@@ -1,64 +1,89 @@
 # Making it compatible for two GPUs -- mostly how its called
-
-import torch
-import os
-
 import gym_super_mario_bros
 from gym_super_mario_bros.actions import RIGHT_ONLY
-
 from agent import Agent
 from nes_py.wrappers import JoypadSpace
 from wrappers import apply_wrappers
-
 from utils import get_current_date_time_string
+import torch
+import torch.distributed as dist
+import torch.multiprocessing as mp
+from torch.nn.parallel import DistributedDataParallel as DDP
+import os
 
-model_path = os.path.join("models", get_current_date_time_string())
-os.makedirs(model_path, exist_ok=True)
+def setup(rank, world_size):
+    os.environ['MASTER_ADDR'] = 'localhost'
+    os.environ['MASTER_PORT'] = '12355'
+    dist.init_process_group("nccl", rank=rank, world_size=world_size)
 
-ENV_NAME = 'SuperMarioBros-1-1-v3'
-SHOULD_TRAIN = True
-DISPLAY = True
-CKPT_SAVE_INTERVAL = 5000
-NUM_OF_EPISODES = 50_000
+def cleanup():
+    dist.destroy_process_group()
 
-env = gym_super_mario_bros.make(ENV_NAME, render_mode='human' if DISPLAY else 'rgb', apply_api_compatibility=True)
-env = JoypadSpace(env, RIGHT_ONLY)
-env = apply_wrappers(env)
+def train(rank, world_size):
+    setup(rank, world_size)
+    
+    model_path = os.path.join("models", get_current_date_time_string())
+    if rank == 0:
+        os.makedirs(model_path, exist_ok=True)
 
-agent = Agent(input_dims=env.observation_space.shape, num_actions=env.action_space.n)
+    ENV_NAME = 'SuperMarioBros-1-1-v3'
+    SHOULD_TRAIN = True
+    DISPLAY = rank == 0  # Only display on main process
+    CKPT_SAVE_INTERVAL = 5000
+    NUM_OF_EPISODES = 50_000
 
-if not SHOULD_TRAIN:
-    folder_name = ""
-    ckpt_name = ""
-    agent.load_model(os.path.join("models", folder_name, ckpt_name))
-    agent.epsilon = 0.2
-    agent.eps_min = 0.0
-    agent.eps_decay = 0.0
+    env = gym_super_mario_bros.make(ENV_NAME, render_mode='human' if DISPLAY else 'rgb', 
+                                  apply_api_compatibility=True)
+    env = JoypadSpace(env, RIGHT_ONLY)
+    env = apply_wrappers(env)
 
-env.reset()
-next_state, reward, done, trunc, info = env.step(action=0)
+    agent = Agent(input_dims=env.observation_space.shape, num_actions=env.action_space.n, rank=rank)
 
-for i in range(NUM_OF_EPISODES):    
-    print("Episode:", i)
-    done = False
-    state, _ = env.reset()
-    total_reward = 0
-    while not done:
-        a = agent.choose_action(state)
-        new_state, reward, done, truncated, info  = env.step(a)
-        total_reward += reward
+    if not SHOULD_TRAIN:
+        folder_name = ""
+        ckpt_name = ""
+        agent.load_model(os.path.join("models", folder_name, ckpt_name))
+        agent.epsilon = 0.2
+        agent.eps_min = 0.0
+        agent.eps_decay = 0.0
 
-        if SHOULD_TRAIN:
-            agent.store_in_memory(state, a, reward, new_state, done)
-            agent.learn()
+    env.reset()
+    next_state, reward, done, trunc, info = env.step(action=0)
 
-        state = new_state
+    for i in range(NUM_OF_EPISODES):    
+        if rank == 0:
+            print("Episode:", i)
+        done = False
+        state, _ = env.reset()
+        total_reward = 0
+        
+        while not done:
+            a = agent.choose_action(state)
+            new_state, reward, done, truncated, info = env.step(a)
+            total_reward += reward
 
-    print("Total reward:", total_reward, "Epsilon:", agent.epsilon, "Size of replay buffer:", len(agent.replay_buffer), "Learn step counter:", agent.learn_step_counter)
+            if SHOULD_TRAIN:
+                agent.store_in_memory(state, a, reward, new_state, done)
+                agent.learn()
 
-    if SHOULD_TRAIN and (i + 1) % CKPT_SAVE_INTERVAL == 0:
-        model_save_path = os.path.join(model_path, f"model_{i + 1}_iter.pt")
-        agent.save_model(model_save_path)
-        print(f"Model saved at: {model_save_path}")
+            state = new_state
 
-env.close()
+        if rank == 0:
+            print("Total reward:", total_reward, "Epsilon:", agent.epsilon, 
+                  "Size of replay buffer:", len(agent.replay_buffer), 
+                  "Learn step counter:", agent.learn_step_counter)
+
+        if SHOULD_TRAIN and (i + 1) % CKPT_SAVE_INTERVAL == 0 and rank == 0:
+            model_save_path = os.path.join(model_path, f"model_{i + 1}_iter.pt")
+            agent.save_model(model_save_path)
+            print(f"Model saved at: {model_save_path}")
+
+        if rank == 0:
+            print("Total reward:", total_reward)
+
+    env.close()
+    cleanup()
+
+if __name__ == "__main__":
+    world_size = 2  # Number of GPUs
+    mp.spawn(train, args=(world_size,), nprocs=world_size)
